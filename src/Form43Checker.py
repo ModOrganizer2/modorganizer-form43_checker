ed8d56a4ebcc82b13e8abef19d31e3d2af2df6c5
AnyOldName3 <krizdjali+github@gmail.com>
2019-12-07 14:31:00 +0000

    Initial commit
    Still to do:
    * Tidy up the plugin file itself (i.e. remove development comments and logging)
    * Make Umbrella-side changes (i.e. add a link to this repo as a build step)
    * Add the translation file to the repo (it should be made the first time this is built with Umbrella)

---
+++ src/Form43Checker.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+import sys
+
+from PyQt5.QtCore import QCoreApplication, qDebug
+
+if "mobase" not in sys.modules:
+    import mock_mobase as mobase
+
+class Form43Checker(mobase.IPluginDiagnose):
+    
+    def __init__(self):
+        super(Form43Checker, self).__init__()
+        self.__organizer = None
+
+    def init(self, organizer):
+        self.__organizer = organizer
+        
+        # Already gets queried whenever a new plugin is added, so that's nice.
+        #organizer.pluginList().onRefreshed(lambda : self._invalidate())
+        #organizer.pluginList().onRefreshed(lambda : qDebug("onRefreshed callback triggered"))
+        
+        return True
+
+    def name(self):
+        return "Form 43 Plugin Checker"
+
+    def author(self):
+        return "AnyOldName3"
+
+    def description(self):
+        return self.__tr("Checks plugins (.ESM/.ESP files) to see if any are Form 43 (Skyrim LE).")
+
+    def version(self):
+        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.prealpha)
+
+    def isActive(self):
+        return self.__organizer.managedGame().gameName() == "Skyrim Special Edition"
+
+    def settings(self):
+        return []
+    
+    def activeProblems(self):
+        qDebug("Active problems called.")
+        if self.__scanPlugins():
+            return [0]
+        else:
+            return []
+    
+    def shortDescription(self, key):
+        return self.__tr("Form 43 plugin detected")
+    
+    def fullDescription(self, key):
+        pluginList = self.__listPlugins()
+        pluginList = [Path(absolutePath).name for absolutePath in pluginList]
+        pluginListString = "\n• " + ("\n• ".join(pluginList))
+        return self.__tr("You have one or more Form 43 plugins. They are:{0}").format(pluginListString)
+    
+    def hasGuidedFix(self, key):
+        return False
+    
+    def startGuidedFix(self, key):
+        # Maybe we could use xEdit or something to resave the file?
+        pass
+    
+    def __tr(self, str):
+        return QCoreApplication.translate("Form43Checker", str)
+    
+    def __testFile(self, path):
+        path = Path(path)
+        if path.is_file():
+            with path.open(mode='rb') as file:
+                file.seek(20)
+                version = file.read(2)
+                return version == b"\x2b\x00"
+        else:
+            qDebug("I've been told to scan a plugin that doesn't exist.")
+            return None
+    
+    def __scanPlugins(self):
+        if self.__organizer.managedGame().gameName() != "Skyrim Special Edition":
+            return False
+        
+        def isPluginFile(name):
+            extension = Path(name).suffix
+            extension = extension.lower()
+            return extension == ".esp" or extension == ".esm"
+        
+        fileList = self.__organizer.findFiles("", isPluginFile)
+        
+        for file in fileList:
+            if self.__testFile(file):
+                return True
+        
+        return False
+    
+    def __listPlugins(self):
+        def isPluginFile(name):
+            extension = Path(name).suffix
+            extension = extension.lower()
+            return extension == ".esp" or extension == ".esm"
+        
+        fileList = self.__organizer.findFiles("", isPluginFile)
+        
+        return [file for file in fileList if self.__testFile(file)]
+                
+    
+def createPlugin():
+    return Form43Checker()
