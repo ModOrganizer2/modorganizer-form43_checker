6d34272b2553311e3b02f782161dc13f69da48eb
Silarn <jrim@rimpo.org>
2020-01-14 00:50:52 -0600

    Check for any form lower than 44 (and output the version)

--- src/Form43Checker.py
+++ src/Form43Checker.py
@@ -23,10 +23,10 @@ class Form43Checker(mobase.IPluginDiagnose):
         return "AnyOldName3"
 
     def description(self):
-        return self.__tr("Checks plugins (.ESM/.ESP files) to see if any are Form 43 (Skyrim LE).")
+        return self.__tr("Checks plugins (.ESM/.ESP files) to see if any are lower than Form 44 (Skyrim SE).")
 
     def version(self):
-        return mobase.VersionInfo(1, 1, 0, mobase.ReleaseType.prealpha)
+        return mobase.VersionInfo(1, 2, 0, mobase.ReleaseType.prealpha)
 
     def isActive(self):
         return (self.__organizer.managedGame().gameName() == "Skyrim Special Edition" and
@@ -44,23 +44,23 @@ class Form43Checker(mobase.IPluginDiagnose):
             return []
 
     def shortDescription(self, key):
-        return self.__tr("Form 43 plugin detected")
+        return self.__tr("Form 43 (or lower) plugin detected")
 
     def fullDescription(self, key):
         pluginList = self.__listPlugins()
         pluginList = [Path(absolutePath).name for absolutePath in pluginList]
         pluginListString = "<br><br>•  " + ("<br>•  ".join(pluginList))
-        outputString = self.__tr("You have one or more form 43 plugins. They are:{0}").format(pluginListString)
+        outputString = self.__tr("You have one or more plugins that are not form 44. They are:{0}").format(pluginListString)
         outputString += "<br><br>"
-        outputString += self.__tr("Form 43 plugins are modules that were made for Skyrim LE (Oldrim) and have not been properly "
-                                  "ported to Skyrim Special Edition, which uses form 44 plugins. This usually results in parts "
-                                  "of the mod not working correctly."
+        outputString += self.__tr("Form 43 (or lower) plugins are modules that were made for Skyrim LE (Oldrim) and have not been "
+                                  "properly ported to Skyrim Special Edition, which uses form 44 plugins. This usually results in "
+                                  "parts of the mod not working correctly."
                                   "<br><br>"
                                   "To be converted, these plugins simply need to be opened and saved with the SSE Creation Kit "
                                   "but their presence can be an indication that a mod was not properly ported to SSE and so "
                                   "can potentially have additional issues." 
                                   "<br><br>"
-                                  "Online guides can have more information on how to correctly convert mods for Skyrim SE. <br>")
+                                  "Online guides can have more information on how to correctly convert mods for Skyrim SE.<br>")
         return outputString
 
     def hasGuidedFix(self, key):
@@ -74,14 +74,20 @@ class Form43Checker(mobase.IPluginDiagnose):
         return QCoreApplication.translate("Form43Checker", str)
 
     def __testFile(self, path):
-        path = Path(path)
+        version = self.__getForm(path)
+        if isinstance(version, int):
+            return version < 44
+        else:
+            return None
+
+    def __getForm(self, file):
+        path = Path(file)
         if path.is_file():
             with path.open(mode='rb') as file:
                 file.seek(20)
-                version = file.read(2)
-                return version == b"\x2b\x00"
+                return int.from_bytes(file.read(2), byteorder='little')
         else:
-            return None
+            return "invalid"
 
     def __scanPlugins(self):
         if self.__organizer.managedGame().gameName() != "Skyrim Special Edition":
@@ -108,7 +114,7 @@ class Form43Checker(mobase.IPluginDiagnose):
 
         fileList = self.__organizer.findFiles("", isPluginFile)
 
-        return [file for file in fileList if self.__testFile(file)]
+        return ["{} (form {})".format(file, self.__getForm(file)) for file in fileList if self.__testFile(file)]
 
 
 def createPlugin():
--- src/Form43Checker_en.ts
+++ src/Form43Checker_en.ts
@@ -4,7 +4,7 @@
     <name>Form43Checker</name>
     <message>
         <location filename="Form43Checker.py" line="26"/>
-        <source>Checks plugins (.ESM/.ESP files) to see if any are Form 43 (Skyrim LE).</source>
+        <source>Checks plugins (.ESM/.ESP files) to see if any are lower than Form 44 (Skyrim SE).</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -14,17 +14,17 @@
     </message>
     <message>
         <location filename="Form43Checker.py" line="47"/>
-        <source>Form 43 plugin detected</source>
+        <source>Form 43 (or lower) plugin detected</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="Form43Checker.py" line="53"/>
-        <source>You have one or more form 43 plugins. They are:{0}</source>
+        <source>You have one or more plugins that are not form 44. They are:{0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="Form43Checker.py" line="55"/>
-        <source>Form 43 plugins are modules that were made for Skyrim LE (Oldrim) and have not been properly ported to Skyrim Special Edition, which uses form 44 plugins. This usually results in parts of the mod not working correctly.&lt;br&gt;&lt;br&gt;To be converted, these plugins simply need to be opened and saved with the SSE Creation Kit but their presence can be an indication that a mod was not properly ported to SSE and so can potentially have additional issues.&lt;br&gt;&lt;br&gt;Online guides can have more information on how to correctly convert mods for Skyrim SE. &lt;br&gt;</source>
+        <source>Form 43 (or lower) plugins are modules that were made for Skyrim LE (Oldrim) and have not been properly ported to Skyrim Special Edition, which uses form 44 plugins. This usually results in parts of the mod not working correctly.&lt;br&gt;&lt;br&gt;To be converted, these plugins simply need to be opened and saved with the SSE Creation Kit but their presence can be an indication that a mod was not properly ported to SSE and so can potentially have additional issues.&lt;br&gt;&lt;br&gt;Online guides can have more information on how to correctly convert mods for Skyrim SE.&lt;br&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
