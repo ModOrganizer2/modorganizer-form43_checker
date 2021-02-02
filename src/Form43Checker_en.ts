cbcbe87a9751914a780966edd4b45b8744f6ccfb
Mikaël Capelle <capelle.mikael@gmail.com>
2021-02-02 20:39:12 +0100

    Update translation file.

--- src/Form43Checker_en.ts
+++ src/Form43Checker_en.ts
@@ -3,27 +3,22 @@
 <context>
     <name>Form43Checker</name>
     <message>
-        <location filename="Form43Checker.py" line="26"/>
+        <location filename="Form43Checker.py" line="29"/>
         <source>Checks plugins (.ESM/.ESP files) to see if any are lower than Form 44 (Skyrim SE).</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Form43Checker.py" line="37"/>
-        <source>Enable plugin</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="Form43Checker.py" line="47"/>
+        <location filename="Form43Checker.py" line="48"/>
         <source>Form 43 (or lower) plugin detected</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Form43Checker.py" line="53"/>
+        <location filename="Form43Checker.py" line="54"/>
         <source>You have one or more plugins that are not form 44. They are:{0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="Form43Checker.py" line="55"/>
+        <location filename="Form43Checker.py" line="56"/>
         <source>Form 43 (or lower) plugins are modules that were made for Skyrim LE (Oldrim) and have not been properly ported to Skyrim Special Edition, which uses form 44 plugins. This usually results in parts of the mod not working correctly.&lt;br&gt;&lt;br&gt;To be converted, these plugins simply need to be opened and saved with the SSE Creation Kit but their presence can be an indication that a mod was not properly ported to SSE and so can potentially have additional issues.&lt;br&gt;&lt;br&gt;Online guides can have more information on how to correctly convert mods for Skyrim SE.&lt;br&gt;</source>
         <translation type="unfinished"></translation>
     </message>
