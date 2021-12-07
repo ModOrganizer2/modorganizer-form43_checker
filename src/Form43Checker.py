7afab41179f19e8df0ea9d9bc995b3d082ebbb54
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2021-12-06 23:42:04 -0600

    Fix pylupdate parsing

--- src/Form43Checker.py
+++ src/Form43Checker.py
@@ -26,7 +26,7 @@ class Form43Checker(mobase.IPluginDiagnose):
         return "AnyOldName3"
 
     def description(self):
-        return self.__tr("Checks plugins (.ESM/.ESP files) to see if any are lower than Form 44 (Skyrim SE).")
+        return self.tr("Checks plugins (.ESM/.ESP files) to see if any are lower than Form 44 (Skyrim SE).")
 
     def version(self):
         return mobase.VersionInfo(1, 2, 0, mobase.ReleaseType.prealpha)
@@ -45,15 +45,15 @@ class Form43Checker(mobase.IPluginDiagnose):
             return []
 
     def shortDescription(self, key):
-        return self.__tr("Form 43 (or lower) plugin detected")
+        return self.tr("Form 43 (or lower) plugin detected")
 
     def fullDescription(self, key):
         pluginList = self.__listPlugins()
         pluginList = [Path(absolutePath).name for absolutePath in pluginList]
         pluginListString = "<br><br>•  " + ("<br>•  ".join(pluginList))
-        outputString = self.__tr("You have one or more plugins that are not form 44. They are:{0}").format(pluginListString)
+        outputString = self.tr("You have one or more plugins that are not form 44. They are:{0}").format(pluginListString)
         outputString += "<br><br>"
-        outputString += self.__tr("Form 43 (or lower) plugins are modules that were made for Skyrim LE (Oldrim) and have not been "
+        outputString += self.tr("Form 43 (or lower) plugins are modules that were made for Skyrim LE (Oldrim) and have not been "
                                   "properly ported to Skyrim Special Edition, which uses form 44 plugins. This usually results in "
                                   "parts of the mod not working correctly."
                                   "<br><br>"
@@ -71,8 +71,8 @@ class Form43Checker(mobase.IPluginDiagnose):
         # Maybe we could use xEdit or something to resave the file?
         pass
 
-    def __tr(self, str):
-        return QCoreApplication.translate("Form43Checker", str)
+    def tr(self, str):
+        return QCoreApplication.tr("Form43Checker", str)
 
     def __testFile(self, path):
         version = self.__getForm(path)
